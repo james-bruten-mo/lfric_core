315a20a590e6f526b78993b66b98435c23d658f7
mikehobson <enquiries@metoffice.gov.uk>
2015-05-20 13:45:43 +0000

    ticket:202 for Matthew Hambley: Fix to compiling the dependency information, which is very, very slow over NFS.
    git-svn-id: https://metomi/svn/lfric.xm/Dynamo/trunk@2904 4bbb03d4-c1cc-11e1-9ca4-71d347b01581

--- src/dynamo/examine.mk
+++ src/dynamo/examine.mk
@@ -13,7 +13,7 @@
 ROOT = ../..
 include $(ROOT)/make/include.mk
 
-DATABASE = $(OBJ_DIR)/dependencies.db
+DYNAMO_DEPENDENCY_DB ?= $(OBJ_DIR)/dependencies.db
 
 ALL_SRC = $(shell find . -name "*.[Ff]90")
 TOUCH_FILES = $(patsubst ./%.f90,$(OBJ_DIR)/%.t,$(patsubst ./%.F90,$(OBJ_DIR)/%.t,$(ALL_SRC)))
@@ -21,21 +21,21 @@ IGNORE_ARGUMENTS := $(patsubst %,-ignore %,$(IGNORE_DEPENDENCIES))
 
 $(OBJ_DIR)/programs.mk: $(OBJ_DIR)/dependencies.mk | $(OBJ_DIR)
 	@echo -e $(VT_BOLD)Building$(VT_RESET) $@
-	$(Q)$(TOOL_DIR)/ProgramObjects -database $(DATABASE) $@
+	$(Q)$(TOOL_DIR)/ProgramObjects -database $(DYNAMO_DEPENDENCY_DB) $@
 
 $(OBJ_DIR)/dependencies.mk: $(TOUCH_FILES) | $(OBJ_DIR) $(OBJ_SUBDIRS)
 	@echo -e $(VT_BOLD)Building$(VT_RESET) $@
-	$(Q)$(TOOL_DIR)/DependencyRules -database $(DATABASE) $@
+	$(Q)$(TOOL_DIR)/DependencyRules -database $(DYNAMO_DEPENDENCY_DB) $@
 
 $(OBJ_DIR)/%.t: %.F90 | $(OBJ_DIR) $(OBJ_SUBDIRS)
 	@echo -e $(VT_BOLD)Analysing$(VT_RESET) $<
 	$(Q)$(TOOL_DIR)/DependencyAnalyser $(IGNORE_ARGUMENTS) \
-	                                   $(DATABASE) $< && touch $@
+	                                   $(DYNAMO_DEPENDENCY_DB) $< && touch $@
 
 $(OBJ_DIR)/%.t: %.f90 | $(OBJ_DIR) $(OBJ_SUBDIRS)
 	@echo -e $(VT_BOLD)Analysing$(VT_RESET) $<
 	$(Q)$(TOOL_DIR)/DependencyAnalyser $(IGNORE_ARGUMENTS) \
-	                                   $(DATABASE) $< && touch $@
+	                                   $(DYNAMO_DEPENDENCY_DB) $< && touch $@
 
 $(OBJ_DIR) $(OBJ_SUBDIRS):
 	@echo -e $(VT_BOLD)Creating$(VT_RESET) $@
