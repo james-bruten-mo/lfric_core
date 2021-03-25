f896b7fec5c6ca42520efa2dba12c74c74a601f6
stevemullerworth <enquiries@metoffice.gov.uk>
2021-03-25 16:17:42 +0000

    ticket:2589 for Steve Wardle: Ensure lfric_atm is picking up fixed revisions of all dependencies
    git-svn-id: https://metomi/svn/lfric.xm/LFRic/trunk@28032 4bbb03d4-c1cc-11e1-9ca4-71d347b01581

--- lfric_atm/fcm-make/parameters.sh
+++ lfric_atm/fcm-make/parameters.sh
@@ -1,13 +1,44 @@
-export compile_atmos=preprocess-atmos
-export config_revision=@vn11.9
-export config_root_path=fcm:um.xm_tr
-export config_type=atmos
-export extract=extract
+# The variables in this file act as overrides to the contents
+# of the UM's fcm-make configuration files in order to extract
+# and preprocess the required code for use in LFRic.
+
+# Revisions and sources for dependent repositories which
+# are extracted as part of the build.  A blank *_sources
+# variable will result in extraction from the project trunk.
+
+# Note that on the LFRic trunk the *_sources variables should
+# always be blank (but can be used on branches during development
+# to enable testing of changes)
+
+# On commit to trunk the code reviewer should ensure all *_sources
+# variables below are empty, and the revisions of any projects with
+# dependent changes should be updated to the revision at which those
+# changes were committed to the project's trunk
+export casim_rev=um11.9  # Note CASIM is not actually
+export casim_sources=    # called in LFRic presently
 export jules_rev=um11.9
 export jules_sources=
-export keys_atmos_extra=LFRIC=lfric
+export shumlib_rev=um11.9
+export shumlib_sources=
 export socrates_rev=um11.9
 export socrates_sources=
-export steplist="extract preprocess-atmos preprocess-recon"
 export um_rev=vn11.9
 export um_sources=
+
+#### Do not edit the definitions below this line without
+#### consulting the owners of this build system
+
+# The revision and location of the build configuration files
+export config_revision=@$um_rev
+export config_root_path=fcm:um.xm_tr
+export config_type=atmos
+
+# Set to extract then preprocess the source only (this is
+# disabling the build of the atmos/recon executables)
+export extract=extract
+export compile_recon=preprocess-recon
+export compile_atmos=preprocess-atmos
+
+# Define any preprocessor macros that are required
+# (in addition to the ones normally used by the UM)
+export keys_atmos_extra=LFRIC=lfric
